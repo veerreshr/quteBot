bd63e670d17941e40de63f664a711e4000307c6d
veeresh <veerreshr@gmail.com>
2020-09-15 22:59:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const axios = require("axios");
+const Telegram = require("node-telegram-bot-api");
+
+const bot = new Telegram(process.env.TELEGRAM_TOKEN);
+
+async function getQuote() {
+  try {
+    const response = await axios.get("https://type.fit/api/quotes");
+    const quote = response[Math.floor(Math.random() * response.length)];
+    return `${quote.text} \n ${quote.author}`;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function sendMessage() {
+  try {
+    const message = await getQuote();
+    bot.sendMessage(process.env.TELEGRAM_CHAT_ID, message);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+sendMessage();
